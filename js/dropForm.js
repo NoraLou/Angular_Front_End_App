1cbbfdd5e9d34027f947e6be93c9a68027863559
nora humpage <nora.humpage@gmail.com>
2015-10-28 16:42:29 -0700

    added toggle function to header, about to refactor controller

--- js/dropForm.js
+++ js/dropForm.js
@@ -6,9 +6,38 @@ angular.module('dropForm', [])
       templateUrl:'drop-form.html',
 
       link: function(scope, elem, attr){
-        var dropButton = angular.element(document.querySelector('#dropButton'));
+
+        var dropButton = angular.element(document.querySelector('#drop-button'));
+
+        var dropForm = angular.element(document.querySelector('#drop-form'));
+
+        scope.dropped = false;
+
+        scope.toggleForm = function(){
+          console.log( 'scope.dropped  ', scope.dropped)
+          if(scope.dropped == false){
+            dropForm.css({'margin-top' : '0px'});
+            scope.dropped = true;
+          }else{
+            dropForm.css({'margin-top' : '-200px'});
+            scope.dropped = false;
+          }
+        }//test
       }
     }
 
   });
+
+
+//var dropForm = elem.find('#drop-form');
+// dropForm.style.webkitTransform= 'translateY(200px)'
+// dropForm.style.transform = 'translateY(200px)'
+
+// dropButton.on('click', function(formToOpen){
+        //   return function(){
+        //     console.log('dropForm  ', dropForm);
+        //     dropForm.style.webkitTransform= 'translateY(200px)'
+
+        // })(dropForm);
+
 
