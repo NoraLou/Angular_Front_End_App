58c50bae34a277fc662dfb6425cb589c05e46591
nora humpage <nora.humpage@gmail.com>
2015-10-30 00:03:41 -0700

    filter works, but getting an error with html syntax

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 (function(){
   var myApp = angular.module('myApp',['ngStorage','ngAnimate','ngRoute',
 
-    'mainController', 'todoService','formController']);
+    'mainController', 'todoService','formController', 'trimWords'])
 
   angular.module('mainController', [])
 
-    .controller('mainController',['$scope', '$localStorage', 'Todos', function($scope, $localStorage, Todos){
+    .controller('mainController',['$scope', '$localStorage', 'Todos', function($scope, $localStorage, Todos, trimWordsFilter){
 
     $scope.$storage = Todos.initStorage;
 
--- js/filter/trimWords.js
+++ js/filter/trimWords.js
@@ -2,8 +2,13 @@ angular.module('trimWords', [])
   .filter('trimWords', function(){
     return function(input, words){
       if(isNaN(words)) return input;
-      if(input)
-    }
-
-  })
+      if(input){
+        var inputWords = input.split(/\s+/);
+          if(inputWords.length > words){
+            input = inputWords.slice(0, words).join(' ') + '...';
+          }
+      }
+      return input;
+    };
+  });
 
